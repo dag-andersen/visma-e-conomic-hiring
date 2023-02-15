6d5a5c72332072cda963a592428ddf733637cd17
Dag Andersen <dagbjerreandersen@gmail.com>
2023-02-15 10:05:31 +0100

    Added health check and prometheus

--- dummy-pdf-or-png/main.go
+++ dummy-pdf-or-png/main.go
@@ -4,11 +4,26 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strings"
 	"time"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func serveRandomFile(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/")
+
+	log.Printf("Request for id: %s", id)
+
+	if id == "0" {
+		http.ServeFile(w, r, "./dummy.pdf")
+		return
+	} else if id == "1" {
+		http.ServeFile(w, r, "./dummy.png")
+		return
+	}
+
 	rnd := rand.Intn(10)
+	log.Printf("Random number: %d", rnd)
 	if rnd < 5 {
 		http.ServeFile(w, r, "./dummy.png")
 		return
@@ -20,9 +35,16 @@ func serveRandomFile(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./corrupt-dummy.pdf")
 }
 
+func serveHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusForbidden)
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	http.HandleFunc("/", serveRandomFile)
+	http.HandleFunc("/health", serveHealthCheck)
+	http.Handle("/metrics", promhttp.Handler())
+
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
